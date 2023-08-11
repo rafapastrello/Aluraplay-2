9922f99a4c98456aa1699050b164d8afa00b2cfc
rafllosilva <125152977+rafllosilva@users.noreply.github.com>
2023-08-11 19:20:33 -0400

    Adicionando arquivos do projeto

---
+++ src/Controller/JsonVideoListController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Entity\Video;
+use Alura\Mvc\Repository\VideoRepository;
+
+class JsonVideoListController implements Controller
+{
+    public function __construct(private VideoRepository $videoRepository)
+    {
+    }
+
+    public function processaRequisicao(): void
+    {
+        $videoList = array_map(function (Video $video): array {
+            return [
+                'url' => $video->url,
+                'title' => $video->title,
+                'file_path' => '/img/uploads/' . $video->getFilePath(),
+            ];
+        }, $this->videoRepository->all());
+        echo json_encode($videoList);
+    }
+}
